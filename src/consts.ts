9662a78cee34e5d40f47d6fc6605c10cafda6327
pdao1 <69880265+pdao1@users.noreply.github.com>
2023-07-08 23:39:07 -1000

    Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Manufactured Consent';
+export const SITE_TITLE_DASH = 'Manufactured-Consent';
+export const SITE_TITLE_DOMAIN = 'manufactured-consent.com';
+export const SITE_DESCRIPTION = `Exposing the American propaganda machine and its threat to the global pursuit of peace`;
