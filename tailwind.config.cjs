7b830c4b15ff153f8083f4c00fe98839e5e55045
pdao1 <69880265+pdao1@users.noreply.github.com>
2023-07-18 21:57:28 -1000

    8 files modified

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,7 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   daisyui: {
-    themes: ['light', 'dark', 'night'],
+    themes: ['light', 'dark', 'night', 'dracula'],
   },
   theme: {
     extend: {},
